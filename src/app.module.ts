89693f869517058df1245f94fdfa41d41c4fb909
xuyaxiong <xyxlindy@163.com>
2024-11-04 13:49:02 +0800

    添加多环境配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import localConfig from './config/local.config';
+import bench1Config from './config/bench1.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './db/database.module';
@@ -16,7 +17,12 @@ import { PlcModule } from './plc/plc.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, load: [localConfig] }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        process.env.platform.trim() === 'bench1' ? bench1Config : localConfig,
+      ],
+    }),
     EventEmitterModule.forRoot(),
     WsModule,
     DatabaseModule,
